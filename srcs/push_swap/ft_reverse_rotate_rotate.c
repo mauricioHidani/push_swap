d48a943d8a6b919da7d5be3ed173deae1e74056e
Mauricio Mityo Hidani <mhidani@c1r5p3.42sp.org.br>
2025-09-30 17:22:51 -0300

    refact: reverse rotate rotate

--- srcs/push_swap/ft_reverse_rotate_rotate.c
+++ srcs/push_swap/ft_reverse_rotate_rotate.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rrr.c                                           :+:      :+:    :+:   */
+/*   ft_reverse_rotate_rotate.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 00:42:12 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/29 00:42:51 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/09/30 15:55:23 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_rrr(t_dlist *stack_a, t_dlist *stack_b)
+void	ft_reverse_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b, char *dsp)
 {
-	ft_rra(stack_a);
-	ft_rrb(stack_b);
+	ft_reverse_rotate(stk_a, NULL);
+	ft_reverse_rotate(stk_b, NULL);
+	ft_putstr_fd(dsp, STDOUT_FILENO);
 }
