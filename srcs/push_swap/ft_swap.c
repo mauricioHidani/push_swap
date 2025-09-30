d1f987c4010640568b1691280f0c9f2252cde054
Mauricio Mityo Hidani <mhidani@c1r5p3.42sp.org.br>
2025-09-30 17:23:36 -0300

    refact: swap

--- srcs/push_swap/ft_swap.c
+++ srcs/push_swap/ft_swap.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sa.c                                            :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 16:35:07 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/29 00:39:31 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/09/30 15:36:52 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_sa(t_dlist **stack_a)
+void	ft_swap(t_dlist **stk, char *dsp)
 {
 	t_dnode	*old_head;
 	t_dnode	*next_old_head;
 
-	if (!stack_a || !(*stack_a)->head || !(*stack_a)->head->next)
+	if (!stk || !(*stk)->head || !(*stk)->head->next)
 		return ;
-	old_head = (*stack_a)->head;
-	next_old_head = (*stack_a)->head->next;
+	old_head = (*stk)->head;
+	next_old_head = (*stk)->head->next;
 	old_head->next = next_old_head->next;
 	if (next_old_head->next)
 		next_old_head->next->prev = old_head;
 	else
-		(*stack_a)->tail = old_head;
+		(*stk)->tail = old_head;
 	next_old_head->prev = NULL;
 	next_old_head->next = old_head;
-	(*stack_a)->head = next_old_head;
+	(*stk)->head = next_old_head;
 	old_head->prev = next_old_head;
+	ft_putstr_fd(dsp, STDOUT_FILENO);
 }
