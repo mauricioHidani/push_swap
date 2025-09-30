ee3d2148155fb60134fe234514599161a556b291
Mauricio Mityo Hidani <mhidani@c1r5p3.42sp.org.br>
2025-09-30 17:22:25 -0300

    refact: rotate rotate

--- srcs/push_swap/ft_rotate_rotate.c
+++ srcs/push_swap/ft_rotate_rotate.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rr.c                                            :+:      :+:    :+:   */
+/*   ft_rotate_rotate.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 00:28:31 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/29 00:29:19 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/09/30 15:35:20 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_rr(t_dlist **stack_a, t_dlist **stack_b)
+void	ft_rotate_rotate(t_dlist **stack_a, t_dlist **stack_b, char *dsp)
 {
-	ft_ra(stack_a);
-	ft_rb(stack_a);
+	ft_rotate(stack_a, NULL);
+	ft_rotate(stack_b, NULL);
+	ft_putstr_fd(dsp, STDOUT_FILENO);
 }
