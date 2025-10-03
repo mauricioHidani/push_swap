96dd1c958897cfbc5b30818fc8a0d051afa0c1b7
mauricioHidani <mhidani@student.42org.br>
2025-10-03 10:38:13 -0300

    refactor: module file movements

--- srcs/push_swap/ft_both_movements.c
+++ srcs/push_swap/ft_both_movements.c
@@ -1,20 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap_swap.c                                     :+:      :+:    :+:   */
+/*   ft_both_movements.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/28 16:51:43 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/30 17:40:16 by mhidani          ###   ########.fr       */
+/*   Created: 2025/10/03 08:55:30 by mhidani           #+#    #+#             */
+/*   Updated: 2025/10/03 08:59:49 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_swap_swap(t_dlist **stk_a, t_dlist **stk_b, char *dsp)
+void	ft_swap_swap(t_dlist **stk_a, t_dlist **stk_b)
 {
 	ft_swap(stk_a, NULL);
 	ft_swap(stk_b, NULL);
-	ft_putstr_fd(dsp, STDOUT_FILENO);
+	ft_putstrln_fd("ss", STDOUT_FILENO);
+}
+
+void	ft_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b)
+{
+	ft_rotate(stk_a, NULL);
+	ft_rotate(stk_b, NULL);
+	ft_putstrln_fd("rr", STDOUT_FILENO);
+}
+
+void	ft_reverse_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b)
+{
+	ft_reverse_rotate(stk_a, NULL);
+	ft_reverse_rotate(stk_b, NULL);
+	ft_putstrln_fd("rrr", STDOUT_FILENO);
 }
---
+++ srcs/push_swap/ft_movements.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_movements.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 08:49:43 by mhidani           #+#    #+#             */
+/*   Updated: 2025/10/03 08:52:23 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_push(t_dlist **tgt, t_dlist **dst, char *dsp)
+{
+	t_dnode	*head_b;
+
+	if (!tgt || !(*tgt)->head)
+		return ;
+	head_b = (*tgt)->head;
+	(*tgt)->head = head_b->next;
+	if ((*tgt)->head)
+		(*tgt)->head->prev = NULL;
+	else
+		(*tgt)->tail = NULL;
+	if ((*dst)->head)
+	{
+		head_b->next = (*dst)->head;
+		(*dst)->head->prev = head_b;
+		(*dst)->head = head_b;
+	}
+	else
+	{
+		(*dst)->head = head_b;
+		(*dst)->tail = head_b;
+		head_b->next = NULL;
+	}
+	(*dst)->size++;
+	(*tgt)->size--;
+	ft_putstrln_fd(dsp, STDOUT_FILENO);
+}
+
+void	ft_rotate(t_dlist **stk, char *dsp)
+{
+	t_dnode	*old_head;
+
+	if (!stk || !(*stk)->head || !(*stk)->tail)
+		return ;
+	if ((*stk)->head == (*stk)->tail)
+		return ;
+	old_head = (*stk)->head;
+	(*stk)->head = old_head->next;
+	(*stk)->head->prev = NULL;
+	old_head->next = NULL;
+	old_head->prev = (*stk)->tail;
+	(*stk)->tail->next = old_head;
+	(*stk)->tail = old_head;
+	ft_putstrln_fd(dsp, STDOUT_FILENO);
+}
+
+void	ft_reverse_rotate(t_dlist **stk, char *dsp)
+{
+	t_dnode	*old_tail;
+
+	if (!stk || !(*stk)->head || !(*stk)->tail)
+		return ;
+	if ((*stk)->head == (*stk)->tail)
+		return ;
+	old_tail = (*stk)->tail;
+	(*stk)->tail = old_tail->prev;
+	(*stk)->tail->next = NULL;
+	(*stk)->head->prev = old_tail;
+	old_tail->next = (*stk)->head;
+	(*stk)->head = old_tail;
+	ft_putstrln_fd(dsp, STDOUT_FILENO);
+}
+
+void	ft_swap(t_dlist **stk, char *dsp)
+{
+	t_dnode	*old_head;
+	t_dnode	*next_old_head;
+
+	if (!stk || !(*stk)->head || !(*stk)->head->next)
+		return ;
+	old_head = (*stk)->head;
+	next_old_head = (*stk)->head->next;
+	old_head->next = next_old_head->next;
+	if (next_old_head->next)
+		next_old_head->next->prev = old_head;
+	else
+		(*stk)->tail = old_head;
+	next_old_head->prev = NULL;
+	next_old_head->next = old_head;
+	(*stk)->head = next_old_head;
+	old_head->prev = next_old_head;
+	ft_putstr_fd(dsp, STDOUT_FILENO);
+}
