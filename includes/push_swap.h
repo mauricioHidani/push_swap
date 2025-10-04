ad8e0b9bc40c6cf5a0ef47b929229f578a384d94
mauricioHidani <mhidani@student.42org.br>
2025-10-04 12:54:52 -0300

    refact: add bool and align to norm

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 23:20:25 by mhidani           #+#    #+#             */
-/*   Updated: 2025/10/02 16:25:09 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/10/04 11:55:47 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,49 @@
 # include "libft.h"
 # include "dlist.h"
 
-typedef struct s_elm
+# define TRUE 0x01
+# define FALSE 0x00
+
+typedef char	t_bool;
+
+typedef struct s_figure
 {
 	int		value;
-	int		rot_cost;
+	int		top_cost;
 	int		closest_factor;
-	char	is_mediam;
-}	t_elm;
+	t_bool	is_mediam;
+}	t_figure;
 
 // Sort Algorithm ==============================================================
-void	ft_stwo(t_dlist **stk);
-void	ft_sthree(t_dlist **stk);
-void	ft_sfive(t_dlist **stk_a, t_dlist **stk_b);
-// Calc Cost -------------------------------------------------------------------
-void	ft_calc_cost(t_dlist *stk, t_dnode *tgt);
-// Utils Calc Cost -------------------------------------------------------------
-char	ft_is_mediam(t_dlist *stk, t_dnode *tgt);
-int		ft_rotate_cost(t_dlist *stk, t_dnode *tgt);
-int		ft_closest_factor(t_dnode *target, int check);
+void		ft_stwo(t_dlist **stk);
+void		ft_sthree(t_dlist **stk);
+void		ft_sfive(t_dlist **stk_a, t_dlist **stk_b);
+// Handle Movement -------------------------------------------------------------
+void		ft_handle_rotate(t_dlist **stk, t_dnode *tgt, char *aim);
+void		ft_handle_bth_rotate(t_dlist **stk_a, t_dlist **stk_b, t_dnode *tb);
+// Cost ------------------------------------------------------------------------
+void		ft_build_cost(t_dlist *stk, t_dnode *tgt);
+t_bool		ft_is_mediam(t_dlist *stk, t_dnode *tgt);
+int			ft_to_top_cost(t_dnode *tgt);
+int			ft_closest_factor(t_dnode *tgt, t_dnode *chk);
 
 // Utils =======================================================================
 // Checkers Order --------------------------------------------------------------
-char	ft_is_ascending(t_dlist *stk);
-char	ft_is_descending(t_dlist *stk);
+t_bool		ft_is_ascending(t_dlist *stk);
+t_bool		ft_is_descending(t_dlist *stk);
 // Checkers end Builder --------------------------------------------------------
-t_dlist	*ft_validate(int argc, char **argv);
-// Cleaner ---------------------------------------------------------------------
-void	ft_clean_element(void *ptr);
+t_dlist		*ft_validate(int argc, char **argv);
+// Methods Element -------------------------------------------------------------
+t_figure	*ft_figure(t_dnode *node);
+void		ft_clean_figure(void *ptr);
 
-// Operations ==================================================================
-// Push ------------------------------------------------------------------------
-void	ft_push(t_dlist **tgt, t_dlist **dst, char *dsp);
-// Swap ------------------------------------------------------------------------
-void	ft_swap(t_dlist **stk, char *dsp);
-void	ft_swap_swap(t_dlist **stk_a, t_dlist **stk_b, char *dsp);
-// Rotate ----------------------------------------------------------------------
-void	ft_rotate(t_dlist **stk, char *dsp);
-void	ft_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b, char *dsp);
-// Reverse Rotate --------------------------------------------------------------
-void	ft_reverse_rotate(t_dlist **stk, char *dsp);
-void	ft_reverse_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b, char *dsp);
+// Movements Operations ========================================================
+void		ft_push(t_dlist **tgt, t_dlist **dst, char *dsp);
+void		ft_swap(t_dlist **stk, char *dsp);
+void		ft_swap_swap(t_dlist **stk_a, t_dlist **stk_b);
+void		ft_rotate(t_dlist **stk, char *dsp);
+void		ft_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b);
+void		ft_reverse_rotate(t_dlist **stk, char *dsp);
+void		ft_reverse_rotate_rotate(t_dlist **stk_a, t_dlist **stk_b);
 
 #endif
