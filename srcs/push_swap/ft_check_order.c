e7b29222d8af9531b67d9a14b517592123172de5
mauricioHidani <mhidani@student.42org.br>
2025-10-04 12:45:02 -0300

    fix: descending

--- srcs/push_swap/ft_check_order.c
+++ srcs/push_swap/ft_check_order.c
@@ -6,40 +6,40 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 00:55:09 by mhidani           #+#    #+#             */
-/*   Updated: 2025/10/02 16:23:54 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/10/03 13:52:29 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-char	ft_is_ascending(t_dlist *stk)
+t_bool	ft_is_ascending(t_dlist *stk)
 {
 	t_dnode	*pvt;
 
 	if (!stk || !stk->head || !stk->tail)
-		return (0);
+		return (FALSE);
 	pvt = stk->head;
 	while (pvt->next)
 	{
-		if (((t_elm *)pvt->data)->value > ((t_elm *)pvt->next->data)->value)
-			return (0);
+		if (ft_figure(pvt)->value > ft_figure(pvt->next)->value)
+			return (FALSE);
 		pvt = pvt->next;
 	}
-	return (1);
+	return (TRUE);
 }
 
-char	ft_is_descending(t_dlist *stk)
+t_bool	ft_is_descending(t_dlist *stk)
 {
 	t_dnode	*pvt;
 
 	if (!stk || !stk->head || !stk->tail)
-		return (0);
+		return (FALSE);
 	pvt = stk->head;
 	while (pvt->next)
 	{
-		if (((t_elm *)pvt->data)->value < ((t_elm *)pvt->next->data)->value)
-			return (0);
+		if (ft_figure(pvt)->value < ft_figure(pvt->next)->value)
+			return (FALSE);
 		pvt = pvt->next;
 	}
-	return (1);
+	return (TRUE);
 }
