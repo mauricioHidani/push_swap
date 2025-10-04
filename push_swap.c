6eab3e53bdd62ed91e9d55ec42bd2866b9348036
mauricioHidani <mhidani@student.42org.br>
2025-10-04 12:52:27 -0300

    refact: display and renamed to figure

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 16:32:48 by mhidani           #+#    #+#             */
-/*   Updated: 2025/10/02 13:41:49 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/10/04 11:48:30 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	main(int argc, char **argv)
 	t_dlist	*stack_b;
 
 	ft_begin_step(argc, argv, &stack_a, &stack_b);
-	// todo: resolve algorithm steps sort stack_a
 	if (ft_is_ascending(stack_a))
 		return (EXIT_FAILURE);
 	if (stack_a->size == 2)
@@ -41,14 +40,14 @@ static void	ft_begin_step(int c, char **v, t_dlist **stk_a, t_dlist **stk_b)
 	*stk_a = ft_validate(c, v);
 	if (!*stk_a)
 	{
-		ft_putstr_fd("Error\n", STDOUT_FILENO);
+		ft_putstrln_fd("Error", STDOUT_FILENO);
 		exit(EXIT_FAILURE);
 	}
 	*stk_b = ft_create_dlist(NULL);
 	if (!*stk_b)
 	{
-		ft_putstr_fd("Error\n", STDOUT_FILENO);
-		ft_clean_dlist(stk_a, ft_clean_element);
+		ft_putstrln_fd("Error", STDOUT_FILENO);
+		ft_clean_dlist(stk_a, ft_clean_figure);
 		exit(EXIT_FAILURE);
 	}
 }
